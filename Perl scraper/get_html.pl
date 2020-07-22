8bd07f98aff527dec287986bdab2849a6db79ef3
teorth <tao@math.ucla.edu>
2020-07-22 14:58:57 -0700

    Got URL formatting right (unless perhaps some XKCD pages have special characters)

--- Perl scraper/get_html.pl
+++ Perl scraper/get_html.pl
@@ -16,7 +16,7 @@ my $page = $mw->get_page( { title => 'List of all comics' } );
 # grab the main text of $page
 my $text = $page->{'*'};
 
-# create a key/value hash for the comic numbers and titles
+# create a key/value hash of hashes for the comic numbers and titles
 my %title_db = ();
 
 for my $line (split /\n/, $text){
@@ -24,7 +24,22 @@ for my $line (split /\n/, $text){
 	# $1 is comic number; $2 is date published; $3 is comic title
 	if ($line =~ /comicsrow\|(.*)\|(.*)\|(.*)\|/)
 	{
-		$title_db{$1} = $3;
+		my $comicnum = $1;
+		
+		# create a hash of various attributes of comic $1 and add it to the %title_db hash
+		my %key = ();
+		$key{"date"} = $2;
+		
+		my $title = $3;
+		$key{"title"} = $title;
+
+		#search and replace, spaces to underscores
+		$title =~ s/ /_/g;
+		
+		print("$title\n");
+		
+		$key{"URL"} = "https://www.explainxkcd.com/wiki/index.php/$comicnum:_$title";
+		$title_db{$1} = \%key;
 	}
 }
 
@@ -32,15 +47,15 @@ for my $line (split /\n/, $text){
 #  print("Comic $_ is titled $title_db{$_}\n");
 #}
 
+
+# Convert %title_db to JSON then dump to file
+
 my $json = JSON->new->allow_nonref;
 my $json_text = $json->encode(\%title_db);
 
 my $titlefilename = ".\\titles.txt";
 my $TITLEFILE;
-
 open($TITLEFILE, "> $titlefilename") or die "Can't open $titlefilename";
-
 print { $TITLEFILE } $json_text;
-
 close($TITLEFILE);
 
