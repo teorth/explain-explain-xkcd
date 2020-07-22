570665a02d4c949cee252dcbeff5f7d6c4b5b063
teorth <tao@math.ucla.edu>
2020-07-21 18:58:02 -0700

    Learned how if statements work

--- Perl scraper/get_html.pl
+++ Perl scraper/get_html.pl
@@ -2,25 +2,27 @@
   
 # Modules used 
 use MediaWiki::API;
- 
+
+
+# activate the explainxkcd API 
 my $mw = MediaWiki::API->new();
 $mw->{config}->{api_url} = 'https://www.explainxkcd.com/wiki/api.php';
  
 
-print "Printing contents\n";
-
 # get some page contents
+# weird issue: can't scrape the list of all comics (full) page, it is too large, so we have to scrape the subpages
 my $page = $mw->get_page( { title => 'List of all comics' } );
 
-# weird issue: can't scrape the list of all comics (full) page, it is too large
-
-# print page contents
-# print $page->{'*'};
-
+# grab the main text of $page
 my $text = $page->{'*'};
 
 for my $line (split /\n/, $text){
-	print ("$1:$2:$3\n") if ($line =~ /comicsrow\|(.*)\|(.*)\|(.*)\|/);
+	# check each line of $text to see if it contains the string "comicsrow|$1|$2|$3|"
+	if ($line =~ /comicsrow\|(.*)\|(.*)\|(.*)\|/)
+	{
+		# $1 is comic number; $2 is date published; $3 is comic title
+		print ("$1:$2:$3\n") 
+	}
 }
 
  
